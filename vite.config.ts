c35e2dcc74772289044cafd17df1629778074ee0
Fábio <fsmaiorano@gmail.com>
2025-02-03 17:04:39 +0000

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
+  build: {
+    outDir: "dist-react",
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+});
