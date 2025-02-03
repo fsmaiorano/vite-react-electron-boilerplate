12340e457cdf9fe8f739b1dfda2408bfddee0b42
Fábio <fsmaiorano@gmail.com>
2025-02-03 19:20:44 +0000

    feat: implement resource statistics polling and static data retrieval in Electron app

--- src/electron/resourceManager.ts
+++ src/electron/resourceManager.ts
@@ -1,17 +1,37 @@
 import osUtils from "os-utils";
-// import fs from "fs";
+import os from "os";
+import fs from "fs";
+import { BrowserWindow } from "electron";
+import { ipcWebContentsSend } from "./util.js";
 
 const POLLING_INTERVAL = 500;
 
-export function pollResources() {
+export function pollResources(mainWindow: BrowserWindow) {
   setInterval(async () => {
     const cpuUsage = await getCpuUsage();
     const ramUsage = getRamUsage();
-    console.log({ cpuUsage, ramUsage });
+    const storageData = getStorageData();
+    ipcWebContentsSend("statistics", mainWindow.webContents, {
+      cpuUsage,
+      ramUsage,
+      storageUsage: storageData.usage,
+    });
   }, POLLING_INTERVAL);
 }
 
-function getCpuUsage() {
+export function getStaticData() {
+  const totalStorage = getStorageData().total;
+  const cpuModel = os.cpus()[0].model;
+  const totalMemoryGB = Math.floor(osUtils.totalmem() / 1024);
+
+  return {
+    totalStorage,
+    cpuModel,
+    totalMemoryGB,
+  };
+}
+
+function getCpuUsage(): Promise<number> {
   return new Promise((resolve) => {
     osUtils.cpuUsage(resolve);
   });
@@ -19,4 +39,16 @@ function getCpuUsage() {
 
 function getRamUsage() {
   return 1 - osUtils.freememPercentage();
+}
+
+function getStorageData() {
+  // requires node 18
+  const stats = fs.statfsSync(process.platform === "win32" ? "C://" : "/");
+  const total = stats.bsize * stats.blocks;
+  const free = stats.bsize * stats.bfree;
+
+  return {
+    total: Math.floor(total / 1_000_000_000),
+    usage: 1 - free / total,
+  };
 }
